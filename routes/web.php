c2b355f4aaf2b7824624daa3a12e74e09aaeb9e9
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-09-15 13:05:10 -0300

    Feat: Adiciona middleware admin nas rotas administrativas

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    // rotas administrativas
+});
+
 require __DIR__.'/auth.php';
