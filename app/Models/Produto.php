4a7251e981f3abfd601bfc5d9bab2a759d1bf4c1
Lucas <lucasamaralsantos07@gmail.com>
2026-09-16 00:11:34 -0300

    feat: Adiciona funções da model Produto

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Produto extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nome', 'descricao', 'preco', 'estoque', 'imagem', 'categoria_id'];
+
+    protected function casts(): array
+    {
+        return [
+            'preco' => 'decimal:2',
+            'estoque' => 'integer',
+        ];
+    }
+
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
