5ead35c29c27114cdabccfa3fb541ce7b134cce2
Lucas <lucasamaralsantos07@gmail.com>
2026-09-16 00:08:04 -0300

    feat: Coluna das tabelas de categoria e produtos

--- database/migrations/2026_09_16_030101_create_produtos_table.php
+++ database/migrations/2026_09_16_030101_create_produtos_table.php
@@ -6,20 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->text('descricao')->nullable();
+            $table->decimal('preco', 10, 2);
+            $table->unsignedInteger('estoque')->default(0);
+            $table->string('imagem')->nullable();
+            $table->foreignId('categoria_id')
+                ->constrained('categorias')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('produtos');
