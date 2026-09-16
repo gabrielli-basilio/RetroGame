72f969d0fc0050d5329a8f9e112ece6ef5d1c183
Lucas <lucasamaralsantos07@gmail.com>
2026-09-16 00:23:06 -0300

    feat: Criação do request StoreProduto

---
+++ app/Http/Requests/StoreProdutoRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProdutoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => ['required', 'string', 'max:255'],
+            'descricao' => ['nullable', 'string'],
+            'preco' => ['required', 'numeric', 'min:0'],
+            'estoque' => ['required', 'integer', 'min:0'],
+            'imagem' => ['nullable', 'image', 'max:2048'],
+            'categoria_id' => ['required', 'exists:categorias,id'],
+        ];
+    }
+}
