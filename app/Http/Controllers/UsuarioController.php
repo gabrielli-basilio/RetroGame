0f442d7862494b3d8349f191fa5df44746c11e5e
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-09-15 22:19:17 -0300

    Feat: Cria UsuarioController com resource

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $usuarios = User::all();
+        return view('usuarios.index', compact('usuarios'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $usuario)
+    {
+        return view('usuarios.edit', compact('usuario'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $usuario)
+    {
+        $request->validate([
+            'role' => 'required|in:cliente,admin',
+        ]);
+
+        $usuario->update([
+            'role' => $request->role,
+        ]);
+
+        return redirect()->route('usuarios.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $usuario)
+    {
+        $usuario->delete();
+        return redirect()->route('usuarios.index');
+    }
+}
